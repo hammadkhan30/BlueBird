903d0bcb6628976fa905d35e8a61f769f33a27ac
Hammad Khan <hammadkhn30@gmail.com>
2020-11-22 00:32:46 +0530

    added database connection and routes connection

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require('express');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+
+const home = require('./routes/home');
 
 const app = express();
+
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+
+mongoose.connect("mongodb://localhost:27017/BlueBirdDB",{useNewUrlParser:true});
+mongoose.set("useCreateIndex",true);
+
+app.get("/",function(req,res){
+  res.render("home");
+})
 
 app.listen(8000,err => {
   if (err) {
