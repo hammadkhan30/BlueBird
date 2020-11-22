bc1a064530cc31cf5810b06044c8487f2194fe76
Hammad Khan <hammadkhn30@gmail.com>
2020-11-23 00:14:58 +0530

    added contact us portion

--- routes/homeroute.js
+++ routes/homeroute.js
@@ -1,8 +1,14 @@
 const express = require('express');
 let router = express.Router();
 
-router.get("",function(req,res){
+router
+.get("",function(req,res){
   res.render("home");
+})
+
+router
+.get("/contactUs",function(req,res){
+  res.render("contact");
 })
 
 module.exports = router;
